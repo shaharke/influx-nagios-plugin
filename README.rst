cd7c6c9e211875f498b9ed8b0acdc9df20a59cca
Shahar Kedar <shahar@gmail.com>
2014-11-22 18:51:25 +0200

    setting up build

---
+++ README.rst
@@ -0,0 +1,4 @@
+nagios-influx-plugin
+====================
+
+Nagios plugin that queries InfluxDB for monitoring stats
--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from distutils.core import setup
 setup(
     name='influx-nagios-plugin',
     version="0.9.0",
-    packages=['influx-nagios-plugin'],
+    packages=['src'],
+    install_requires=['influxdb', 'NagAconda'],
     license='MIT',
     url="https://github.com/shaharke/nagios-influx-plugin.git",
     description='Nagios plugin for querying monitoring stats from InfluxDB',
     author='Shahar Kedar',
     author_email='shahar at gmail com',
-    scripts=['check_influx.py']
+    scripts=['src/check_influx']
 )
